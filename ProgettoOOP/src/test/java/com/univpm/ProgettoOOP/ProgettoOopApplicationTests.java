a0c5bcefefa9fac32afe5c566035ca29bb87c137
Nicola Ricciardi <nicolaricciardi97@gmail.com>
2020-05-19 16:02:00 +0200

    Creazione Progetto OOP
    Creazione della cartella contenente i file per far girare il programma
    SpringBoot con le relative dipendenze (POM).
    
    Signed-off-by: Nicola Ricciardi <nicolaricciardi97@gmail.com>

---
+++ ProgettoOOP/src/test/java/com/univpm/ProgettoOOP/ProgettoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univpm.ProgettoOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
