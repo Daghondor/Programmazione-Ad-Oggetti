b16510a482062db1ec498809a4d92b5a7a7738a9
Nicola Ricciardi <nicolaricciardi97@gmail.com>
2020-06-10 21:16:13 +0200

    Add Comment JavaDoc

--- ProgettoOOP/src/main/java/com/univpm/ProgettoOOP/Exception/TweetException.java
+++ ProgettoOOP/src/main/java/com/univpm/ProgettoOOP/Exception/TweetException.java
@@ -7,6 +7,14 @@ package com.univpm.ProgettoOOP.Exception;
  */
 public class TweetException extends Exception
 {
+	/**
+	 * Numero che viene utilizzato durante la deserializzazione per verificare che il mittente 
+	 * e il destinatario di un oggetto serializzato abbiano caricato classi per quell'oggetto 
+	 * compatibili rispetto alla serializzazione. 
+	 * Se il destinatario ha caricato una classe per l'oggetto che ha una diversa 
+	 * serialVersionUIDda quella della corrispondente classe del mittente, la deserializzazione 
+	 * si tradurrà in un InvalidClassException. 
+	 */
 	private static final long serialVersionUID = 1L;
 
 	/**
